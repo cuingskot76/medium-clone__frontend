43352dce9836b612aa538f41d23b79ab78df5f59
cuingskot76 <afrizapamujo@gmail.com>
2023-09-04 08:39:23 +0700

    [chore]: change logo Medium

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import MediumLogo from "../public/images/logo.png";
+import MediumLogo from "../public/images/logo.svg";
 import Link from "next/link";
 
 const Navbar = () => {
