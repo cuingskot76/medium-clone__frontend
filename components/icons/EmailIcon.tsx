2ad9af483e7c75ad8d3c114d17aef3098906a42c
cuingskot76 <afrizapamujo@gmail.com>
2023-09-04 23:11:24 +0700

    [chore]: add icons for SignIn

---
+++ components/icons/EmailIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EmailIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <g stroke="#242424">
+        <rect x="3.5" y="5.5" width="17" height="13" rx="1"></rect>
+        <path d="M3.5 8l8.5 6 8.5-6" stroke-linecap="round"></path>
+      </g>
+    </svg>
+  );
+};
+
+export default EmailIcon;
