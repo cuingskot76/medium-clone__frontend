76ce3ea4cbb13bb918455f349cd19a2c1371dc50
cuingskot76 <afrizapamujo@gmail.com>
2023-09-05 23:27:56 +0700

    [chore]: convert .svg icon into component

---
+++ components/icons/ChevronDownIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ChevronDownIcon = () => {
+  return (
+    <svg width="12px" height="12px" viewBox="0 0 15 15">
+      <path
+        d="M3.85 5.15a.5.5 0 0 0-.7.7l4.35 4.36 4.35-4.36a.5.5 0 1 0-.7-.7L7.5 8.79 3.85 5.15z"
+        fill-rule="evenodd"
+      ></path>
+    </svg>
+  );
+};
+
+export default ChevronDownIcon;
