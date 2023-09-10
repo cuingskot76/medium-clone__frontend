2a343d26133ee1afeded942418cb53bc4bcd0ac4
cuingskot76 <afrizapamujo@gmail.com>
2023-09-10 09:56:20 +0700

    [chore]: fix warning default style, add Bookmark icon

--- components/icons/EmailIcon.tsx
+++ components/icons/EmailIcon.tsx
@@ -5,7 +5,7 @@ const EmailIcon = () => {
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
       <g stroke="#242424">
         <rect x="3.5" y="5.5" width="17" height="13" rx="1"></rect>
-        <path d="M3.5 8l8.5 6 8.5-6" stroke-linecap="round"></path>
+        <path d="M3.5 8l8.5 6 8.5-6" strokeLinecap="round"></path>
       </g>
     </svg>
   );
