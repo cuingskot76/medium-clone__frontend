7f27d6612dcfaa5bf2608ca86068534df7df99c6
cuingskot76 <afrizapamujo@gmail.com>
2023-09-10 21:19:35 +0700

    [feat]: change background-color when scrolling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import MediumLogo from "../public/images/logo.svg";
 import MediumArtboard from "../public/images/artboard.svg";
@@ -15,11 +15,29 @@ import SignUp from "./Auth/SignUp";
 const Navbar = () => {
   const [isLogin, setIsLogin] = useState(false);
   const [isRegister, setIsRegister] = useState(false);
+  const [scrollPos, setScrollPos] = useState(false);
 
   let decodeToken = null;
 
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const maxScroll = currentScrollPos > 380;
+    setScrollPos(maxScroll);
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [scrollPos]);
+
   return (
-    <section className="bg-yellow border-b border-dark">
+    <header>
       {decodeToken ? (
         <section className="px-6 py-1 border-b border-[#F2F2F2] flex">
           <div className="gap-4 flex flex-1">
@@ -56,66 +74,71 @@ const Navbar = () => {
         </section>
       ) : (
         <section
-          className={`relative ${!isRegister && !isLogin && "max-w-screen-xl"} 
-        px-7 md:px-12 m-auto`}
+          className={`z-50 ${
+            scrollPos ? "bg-white" : "bg-yellow"
+          } mx-auto w-full border-b border-dark fixed top-0 left-0 right-0 h-20`}
         >
-          <div className="flex justify-between items-center h-20">
-            <Image
-              src={MediumLogo}
-              alt="Medium Logo"
-              width={160}
-              height={160}
-            />
-            <nav className="flex items-center">
-              <div className="hidden md:flex gap-6">
-                <Link href="/" className="text-sm">
-                  Our story
-                </Link>
-                <Link href="/" className="text-sm">
-                  Membership
-                </Link>
-                <Link href="/" className="text-sm">
-                  Write
-                </Link>
-              </div>
+          <div className={`max-w-screen-xl  mx-auto px-7 md:px-12 m-auto`}>
+            <div className="flex justify-between items-center h-20">
+              <Image
+                src={MediumLogo}
+                alt="Medium Logo"
+                width={160}
+                height={160}
+              />
+              <nav className="flex items-center">
+                <div className="hidden md:flex gap-6">
+                  <Link href="/" className="text-sm">
+                    Our story
+                  </Link>
+                  <Link href="/" className="text-sm">
+                    Membership
+                  </Link>
+                  <Link href="/" className="text-sm">
+                    Write
+                  </Link>
+                </div>
 
-              <div className=" flex items-center gap-6 ml-6">
-                <p
-                  className={`text-sm ${!isLogin && "cursor-pointer"}`}
-                  onClick={() => {
-                    setIsLogin(true);
-                  }}
-                >
-                  Sign In
-                  {isLogin && (
-                    <SignIn
-                      setIsLogin={setIsLogin}
-                      setIsRegister={setIsRegister}
-                    />
-                  )}
-                </p>
-                <p
-                  className={`text-sm rounded-full text-start ${
-                    !isRegister && "cursor-pointer"
-                  } bg-dark px-4 py-2`}
-                  onClick={() => {
-                    setIsRegister(true);
-                  }}
-                >
-                  <span className="text-white">Get started</span>
-                  {isRegister && (
-                    <SignUp
-                      setIsRegister={setIsRegister}
-                      setIsLogin={setIsLogin}
-                    />
-                  )}
-                </p>
-              </div>
-            </nav>
+                <div className=" flex items-center gap-6 ml-6">
+                  <p
+                    className={`text-sm ${!isLogin && "cursor-pointer"}`}
+                    onClick={() => {
+                      setIsLogin(true);
+                    }}
+                  >
+                    Sign In
+                    {isLogin && (
+                      <SignIn
+                        setIsLogin={setIsLogin}
+                        setIsRegister={setIsRegister}
+                      />
+                    )}
+                  </p>
+                  <p
+                    className={`text-sm rounded-full text-start ${
+                      !isRegister && "cursor-pointer"
+                    } ${
+                      scrollPos ? "bg-[#1A8917] hover:bg-[#1d6b1a]" : "bg-dark"
+                    } px-4 py-2`}
+                    onClick={() => {
+                      setIsRegister(true);
+                    }}
+                  >
+                    <span className="text-white">Get started</span>
+                    {isRegister && (
+                      <SignUp
+                        setIsRegister={setIsRegister}
+                        setIsLogin={setIsLogin}
+                      />
+                    )}
+                  </p>
+                </div>
+              </nav>
+            </div>
           </div>
         </section>
       )}
-    </section>
+    </header>
   );
 };
 
