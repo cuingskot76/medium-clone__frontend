9c16204a10c120c5e216f8abd440fd5d550a7275
cuingskot76 <afrizapamujo@gmail.com>
2023-09-16 11:43:46 +0700

    [feat]: add signIn with email and password, connect to db

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,76 +1,57 @@
-// import type { NextAuthOptions } from "next-auth";
-// import GitHubProvider from "next-auth/providers/github";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { GithubProfile } from "next-auth/providers/github";
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-// export const options: NextAuthOptions = {
-//   providers: [
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         email: {
-//           label: "Email:",
-//           type: "email",
-//           placeholder: "your-cool-username",
-//         },
-//         password: {
-//           label: "Password:",
-//           type: "password",
-//           placeholder: "your-awesome-password",
-//         },
-//       },
-//       async authorize(credentials) {
-//         // This is where you need to retrieve user data
-//         // to verify with credentials
-//         // Docs: https://next-auth.js.org/configuration/providers/credentials
-//         // const user = {
-//         //   id: "42",
-//         //   name: "Dave",
-//         //   password: "nextauth",
-//         //   role: "manager",
-//         // };
+export const options: NextAuthOptions = {
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email:",
+          type: "email",
+          placeholder: "your-cool-username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "your-awesome-password",
+        },
+      },
+      async authorize(credentials) {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/users/login`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          }
+        );
 
-//         // if (
-//         //   credentials?.username === user.name &&
-//         //   credentials?.password === user.password
-//         // ) {
-//         //   return user;
-//         // } else {
-//         //   return null;
-//         // }
+        const data = await res.json();
 
-//         const res = await fetch(
-//           `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/users/login`,
-//           {
-//             method: "POST",
-//             headers: {
-//               "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({
-//               email: credentials?.email,
-//               password: credentials?.password,
-//             }),
-//           }
-//         );
-
-//         const data = await res.json();
-//         console.log("data", data);
-
-//         if (data) {
-//           return data;
-//         } else {
-//           return null;
-//         }
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     async jwt({ token, user }) {
-//       return { ...token, ...user };
-//     },
-//     async session({ session, token, user }) {
-//       session.user = token as any;
-//       return session;
-//     },
-//   },
-// };
+        if (data) {
+          return data;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+};
