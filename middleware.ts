e2e2c585f3cbd69e1a3ca8a56fa144a619d2b04c
cuingskot76 <afrizapamujo@gmail.com>
2023-09-17 15:01:05 +0700

    [chore]: add protect route

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,21 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const cookie = req.cookies.get("jwt");
+  const cookie = req.cookies.get("next-auth.session-token");
+  console.log("cookie", cookie);
 
   // const baseUrl = req.nextUrl.pathname.startsWith("/");
   // console.log("baseUrl", baseUrl);
 
-  if (!cookie && req.nextUrl.pathname.startsWith("/dashboard")) {
-    // const url = req.nextUrl.clone();
-    // url.pathname = "/";
-    // console.log("url", url);
+  // if (!cookie && req.nextUrl.pathname.startsWith("/dashboard")) {
+  //   // const url = req.nextUrl.clone();
+  //   // url.pathname = "/";
+  //   // console.log("url", url);
 
-    // Redirect to login if no cookie is set
-    return NextResponse.rewrite(new URL("/", req.nextUrl));
-    // return NextResponse.rewrite(url.pathname);
-  }
+  //   // Redirect to login if no cookie is set
+  //   return NextResponse.rewrite(new URL("/", req.nextUrl));
+  //   // return NextResponse.rewrite(url.pathname);
+  // }
 
   if (cookie) {
     // redirect to dashboard if cookie is set
@@ -25,7 +26,19 @@ export function middleware(req: NextRequest) {
     return NextResponse.rewrite(new URL("/dashboard", req.nextUrl));
   }
 
-  return NextResponse.next();
+  if (!cookie) {
+    // redirect to dashboard if cookie is set
+    // const url = req.nextUrl.clone();
+    // url.pathname = "/dashboard";
+
+    return NextResponse.rewrite(new URL("/", req.nextUrl));
+  }
+
+  if (req.nextUrl.pathname.startsWith("/dashboard") && !cookie) {
+    return NextResponse.rewrite(new URL("/", req.nextUrl));
+  }
+
+  // return NextResponse.next();
 }
 
 export const config = {
