5eecaec8fd84ca3d990fc43c0aa9cb5111cb9bb2
cuingskot76 <afrizapamujo@gmail.com>
2023-09-17 13:14:38 +0700

    [chore]: add content dummyTrending blog

--- constants/index.js
+++ constants/index.js
@@ -66,48 +66,94 @@ const discoverTags = [
 
 const dummyTrendings = [
   {
+    id: 2345,
     title: "The 5 Best Ways to Build Resiliency",
     author: "Darius Foroux",
     image: DefaultImage,
     readTime: "3 min read",
     date: "Nov 2",
+    authorFollowers: "123K",
+    like: "23.4K",
+    comment: "235",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam. Quis lectus nulla at volutpat diam. Sed blandit libero volutpat sed. Diam donec adipiscing tristique risus nec feugiat in. Justo eget magna fermentum iaculis eu non. At elementum eu facilisis sed odio morbi. In aliquam sem fringilla ut. Ultrices dui sapien eget mi. Vitae tortor condimentum lacinia quis vel. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Tincidunt tortor aliquam nulla facilisi cras. Ornare arcu dui vivamus arcu. Diam sollicitudin tempor id eu nisl. Elit ut aliquam purus sit amet luctus. Purus in mollis nunc sed id semper risus in hendrerit. Nunc lobortis mattis aliquam faucibus purus in. Vitae suscipit tellus mauris a diam maecenas sed enim. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Purus in massa tempor nec feugiat nisl pretium. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Commodo sed egestas egestas fringilla phasellus. A pellentesque sit amet porttitor eget dolor.",
   },
   {
+    id: 2345,
     title: "Why I’m Not a Fan of the “Fake It Till You Make It” Approach",
     author: "Ayodeji Awosika",
     image: DefaultImage,
     readTime: "5 min read",
     date: "Nov 5",
+    authorFollowers: "12K",
+    like: "23.4K",
+    comment: "235",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam. Quis lectus nulla at volutpat diam. Sed blandit libero volutpat sed. Diam donec adipiscing tristique risus nec feugiat in. Justo eget magna fermentum iaculis eu non. At elementum eu facilisis sed odio morbi. In aliquam sem fringilla ut. Ultrices dui sapien eget mi. Vitae tortor condimentum lacinia quis vel. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Tincidunt tortor aliquam nulla facilisi cras. Ornare arcu dui vivamus arcu. Diam sollicitudin tempor id eu nisl. Elit ut aliquam purus sit amet luctus. Purus in mollis nunc sed id semper risus in hendrerit. Nunc lobortis mattis aliquam faucibus purus in. Vitae suscipit tellus mauris a diam maecenas sed enim. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Purus in massa tempor nec feugiat nisl pretium. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Commodo sed egestas egestas fringilla phasellus. A pellentesque sit amet porttitor eget dolor.",
+    descriptionAuthor:
+      " Volutpat odio facilisis mauris sit amet massa vitae tortor. Rhoncus urna neque viverra justo. Sit amet justo donec enim diam vulputate ut pharetra sit. Gravida quis blandit turpis cursus. ",
   },
   {
+    id: 2345,
     title: "The Chatbot Revolution is Here",
     author: "Chatbots Magazine",
     image: DefaultImage,
     readTime: "8 min read",
     date: "Nov 1",
+    authorFollowers: "3K",
+    like: "23.4K",
+    comment: "235",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam. Quis lectus nulla at volutpat diam. Sed blandit libero volutpat sed. Diam donec adipiscing tristique risus nec feugiat in. Justo eget magna fermentum iaculis eu non. At elementum eu facilisis sed odio morbi. In aliquam sem fringilla ut. Ultrices dui sapien eget mi. Vitae tortor condimentum lacinia quis vel. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Tincidunt tortor aliquam nulla facilisi cras. Ornare arcu dui vivamus arcu. Diam sollicitudin tempor id eu nisl. Elit ut aliquam purus sit amet luctus. Purus in mollis nunc sed id semper risus in hendrerit. Nunc lobortis mattis aliquam faucibus purus in. Vitae suscipit tellus mauris a diam maecenas sed enim. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Purus in massa tempor nec feugiat nisl pretium. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Commodo sed egestas egestas fringilla phasellus. A pellentesque sit amet porttitor eget dolor.",
+    descriptionAuthor:
+      "Tempor id eu nisl nunc mi. Auctor elit sed vulputate mi sit amet mauris. Urna condimentum mattis pellentesque id nibh tortor id aliquet.",
     isPremium: <StarIcon />,
   },
   {
+    id: 2345,
     title: "Good Design is Imperfect Design",
     author: "Muzli - Design Inspiration",
     image: DefaultImage,
     readTime: "5 min read",
     date: "Nov 3",
+    authorFollowers: "4K",
+    like: "23.4K",
+    comment: "235",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam. Quis lectus nulla at volutpat diam. Sed blandit libero volutpat sed. Diam donec adipiscing tristique risus nec feugiat in. Justo eget magna fermentum iaculis eu non. At elementum eu facilisis sed odio morbi. In aliquam sem fringilla ut. Ultrices dui sapien eget mi. Vitae tortor condimentum lacinia quis vel. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Tincidunt tortor aliquam nulla facilisi cras. Ornare arcu dui vivamus arcu. Diam sollicitudin tempor id eu nisl. Elit ut aliquam purus sit amet luctus. Purus in mollis nunc sed id semper risus in hendrerit. Nunc lobortis mattis aliquam faucibus purus in. Vitae suscipit tellus mauris a diam maecenas sed enim. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Purus in massa tempor nec feugiat nisl pretium. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Commodo sed egestas egestas fringilla phasellus. A pellentesque sit amet porttitor eget dolor.",
+    descriptionAuthor:
+      " Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel.",
   },
   {
+    id: 2345,
     title: "Ukraine is the Wild West of Cryptocurrency",
     author: "Alex Gladstein",
     image: DefaultImage,
     readTime: "5 min read",
     date: "Nov 4",
+    authorFollowers: "233",
+    like: "23.4K",
+    comment: "235",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam. Quis lectus nulla at volutpat diam. Sed blandit libero volutpat sed. Diam donec adipiscing tristique risus nec feugiat in. Justo eget magna fermentum iaculis eu non. At elementum eu facilisis sed odio morbi. In aliquam sem fringilla ut. Ultrices dui sapien eget mi. Vitae tortor condimentum lacinia quis vel. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Tincidunt tortor aliquam nulla facilisi cras. Ornare arcu dui vivamus arcu. Diam sollicitudin tempor id eu nisl. Elit ut aliquam purus sit amet luctus. Purus in mollis nunc sed id semper risus in hendrerit. Nunc lobortis mattis aliquam faucibus purus in. Vitae suscipit tellus mauris a diam maecenas sed enim. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Purus in massa tempor nec feugiat nisl pretium. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Commodo sed egestas egestas fringilla phasellus. A pellentesque sit amet porttitor eget dolor.",
+    descriptionAuthor:
+      " Enim blandit volutpat maecenas volutpat blandit aliquam etiam. In arcu cursus euismod quis viverra nibh cras pulvinar. ",
     isPremium: <StarIcon />,
   },
   {
+    id: 2345,
     title: "The 'Male Loneliness Epidemic' Douesn't Exist",
     author: "Jude Ellison Sady Doyle",
     image: DefaultImage,
     readTime: "8 min read",
     date: "Sep 1",
+    authorFollowers: "13K",
+    like: "23.4K",
+    comment: "235",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam. Quis lectus nulla at volutpat diam. Sed blandit libero volutpat sed. Diam donec adipiscing tristique risus nec feugiat in. Justo eget magna fermentum iaculis eu non. At elementum eu facilisis sed odio morbi. In aliquam sem fringilla ut. Ultrices dui sapien eget mi. Vitae tortor condimentum lacinia quis vel. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Tincidunt tortor aliquam nulla facilisi cras. Ornare arcu dui vivamus arcu. Diam sollicitudin tempor id eu nisl. Elit ut aliquam purus sit amet luctus. Purus in mollis nunc sed id semper risus in hendrerit. Nunc lobortis mattis aliquam faucibus purus in. Vitae suscipit tellus mauris a diam maecenas sed enim. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Purus in massa tempor nec feugiat nisl pretium. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Commodo sed egestas egestas fringilla phasellus. A pellentesque sit amet porttitor eget dolor.",
+    descriptionAuthor:
+      "Dignissim enim sit amet venenatis urna. Aliquet porttitor lacus luctus accumsan tortor posuere.",
     isPremium: <StarIcon />,
   },
 ];
