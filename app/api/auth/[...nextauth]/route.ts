46c58e25168646c11faabb79eaedfd7e3953aabd
cuingskot76 <afrizapamujo@gmail.com>
2023-09-20 16:58:13 +0700

    [feat]: add login auth with Google, and connect with prisma adapter, and store user data into database

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,79 +1,19 @@
 import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+const prisma = new PrismaClient();
 
 export const authOption: NextAuthOptions = {
-  pages: {
-    signIn: "/",
-  },
+  adapter: PrismaAdapter(prisma),
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email:",
-          type: "email",
-          placeholder: "your-cool-username",
-        },
-        password: {
-          label: "Password:",
-          type: "password",
-          placeholder: "your-awesome-password",
-        },
-      },
-      async authorize(credentials) {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/users/login`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
-          }
-        );
-
-        const data = await res.json();
-
-        if (data) {
-          return data;
-        } else {
-          return null;
-        }
-      },
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        return {
-          ...token,
-          id: user.id,
-          email: user.email,
-          username: user.username,
-        };
-      }
-      return token;
-    },
-
-    async session({ session, token }) {
-      if (token) {
-        return {
-          ...session,
-          user: {
-            ...session.user,
-            id: token.id,
-            email: token.email,
-            username: token.username,
-          },
-        };
-      }
-      return session;
-    },
-  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
