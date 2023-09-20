a6dee140019ab58133cc469a997376cf8c3c04e0
cuingskot76 <afrizapamujo@gmail.com>
2023-09-20 16:56:23 +0700

    [feat]: add domain image from Google

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["github.com", "miro.medium.com"],
+    domains: ["lh3.googleusercontent.com", "github.com", "miro.medium.com"],
   },
 };
 
