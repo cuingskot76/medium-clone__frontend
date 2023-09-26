5879109c5a41752496dd2e88a1197d6167d3f3f3
cuingskot76 <afrizapamujo@gmail.com>
2023-09-26 22:13:41 +0700

    [feat]: remove login with creadentials, add login with Google

--- components/Auth/SignIn.tsx
+++ components/Auth/SignIn.tsx
@@ -38,30 +38,9 @@ const sigInOptions = [
 ];
 
 const SignIn = ({ setIsLogin, setIsRegister }: any) => {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  const [error, setError] = useState("");
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
-    try {
-      signIn("credentials", {
-        email: user.email,
-        password: user.password,
-        redirect: true,
-        callbackUrl: "/dashboard",
-      });
-    } catch (error) {
-      setError(axiosErrorHandling(error));
-    }
-  };
-
   return (
     <section
-      className="absolute z-50 top-0 right-0 left-0 transparent-bg h-screen m-auto md:flex"
+      className=" fixed z-50 top-0 right-0 left-0 bottom-0 transparent-bg overflow-hidden h-screen m-auto md:flex"
       onClick={(e) => {
         if (e.target === e.currentTarget) {
           e.stopPropagation();
@@ -71,75 +50,11 @@ const SignIn = ({ setIsLogin, setIsRegister }: any) => {
         }
       }}
     >
-      <div className="md:max-w-2xl w-full lg:h-[820px] bg-white shadow-lg py-11 px-14 h-screen m-auto">
+      <div className="h-screen w-full items-center flex md:max-w-xl lg:h-fit bg-white shadow-lg py-11 px-14 m-auto">
         <div>
-          <p className={`text-3xl text-center ${myFont.className} mb-12`}>
+          <p className={`text-3xl text-center ${myFont.className} mb-14`}>
             Welcome back.
           </p>
-
-          <form className="space-y-3 text-start" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-neutral-600"
-              >
-                Email address
-              </label>
-              <div className="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  placeholder="Your Email"
-                  className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                  value={user.email}
-                  onChange={(e) => setUser({ ...user, email: e.target.value })}
-                />
-                {error && <p className="text-red-500 mt-1 text-sm">{error}</p>}
-              </div>
-            </div>
-
-            <div className="space-y-1">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-neutral-600"
-              >
-                Password
-              </label>
-              <div className="mt-1">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  placeholder="Your Password"
-                  className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                  value={user.password}
-                  onChange={(e) =>
-                    setUser({ ...user, password: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-
-            <div>
-              <Button className="flex w-full items-center justify-center px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                Sign in
-              </Button>
-            </div>
-          </form>
-
-          <div className="relative my-6">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-300"></div>
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="px-2 text-neutral-600 bg-white">
-                Or continue with
-              </span>
-            </div>
-          </div>
 
           <div className="flex justify-center flex-col items-center gap-3">
             {sigInOptions.map((option) => (
@@ -160,7 +75,12 @@ const SignIn = ({ setIsLogin, setIsRegister }: any) => {
                     option.icon
                   )}
                 </div>
-                <span className="ml-2">Sign in with {option.name}</span>
+                <span
+                  className="ml-2"
+                  onClick={() => option.name === "Google" && signIn("google")}
+                >
+                  Sign in with {option.name}
+                </span>
               </Button>
             ))}
           </div>
